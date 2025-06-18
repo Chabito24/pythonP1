3322cef025d7eb66e040796d17e842a6f9f4b9fd
Edward Martinez <emartinezj1802@gmail.com>
2025-06-18 15:08:04 -0500

    se agrega comentario

--- conocimientos_1.py
+++ conocimientos_1.py
@@ -1,1 +1,6 @@
-print(" el nombre de tu cerveza es:\n \t" + (input("anño de nacimiento: ")) + " " +(input("Color favorito en ingles: ")))
+print(" el nombre de tu cerveza es:\n \t" + (input("año de nacimiento: ")) + " " +(input("Color favorito en ingles: ")))
+
+"""
+Se realiza ejercicio donde se requiere por medio de dos preguntas asignar el nombre de una cerveza
+en este caso se pregunta el año de nacimiento y un color en ingles lo cual imprime el resultado en dos lineas gracias al \n y una tabulacion en la segunda linea con el \t
+"""
