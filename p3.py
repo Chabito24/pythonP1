c74517ff56fc61b9b01c9659281747b685520e31
Edward Martinez <emartinezj1802@gmail.com>
2025-06-18 10:55:13 -0500

    tercer proyecto de Python

---
+++ p3.py
@@ -0,0 +1,19 @@
+# EJERCICIO 1 #
+from xml.dom.minidom import ProcessingInstruction
+
+input("tu nombre ") # capturamos en este caso el nombre del usuario #
+print(input("tu animal favorito ")) # imprimimos el imput en este caso de tu animal favorito #
+
+# EJERCICIO 2 #
+
+input("dime tu nombre ") # solo captura el nombre #
+input("dime tu apellido ") # solo captura el apellido #
+
+# EJERCICIO 3 #
+
+print(input("dime tu nombre ")) # captura y luego imprime #
+print(input("dime tu apellido ")) # captura y luego imprime #
+
+# EJERCICIO 4 #
+
+print("tu nombre es: " + (input("dime tu nombre ")) + " " + (input("dime tu apellido "))) # Captura el nombre, luego el apellido y despues muestra la impresion diciendo tu nombre es: XXXXX XXXXX #
