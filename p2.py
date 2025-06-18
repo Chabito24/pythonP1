e137409f76f6d0ebe5b177364bd4d2415edb662d
Edward Martinez <emartinezj1802@gmail.com>
2025-06-18 10:31:30 -0500

    segundo proyecto de python

---
+++ p2.py
@@ -0,0 +1,7 @@
+print('Hola' + 'Edward') #aqui concatenamos dos cadenas de texto podemos ver que quedan unidas las cadenas por qe no hay espacios enter las cademas#
+print('hola ' + 'Edward') #aqui se concatenan nuevamante pero ya hay un espacio en la cadena de texto del 'hola'#
+print('hola' + ' ' + 'Edward') #aqui se agrego una tercera cadena de texto con el espacio  y concatenando#
+print("Me llamo \"Edward\"") # puedo incluir comillas dobles dendtro de comillas dobles o sencillas dentro de comillas sencillas con la barra invertida permite tomar el siguiente caracter como un caracter especial #
+print("esta es una linea \nY esta es ootra linea") # Esto me permite esscribir es una misma cadena de texto dos o mas lineas separadas una de la otra con \n #
+print('\tEsta es la tercera linea') # esto me agrega una tabulacion #
+print('This isn\'t a number')
