2c334b649dd16ee90752e68281bca64576dde9c4
Edward Martinez <emartinezj1802@gmail.com>
2025-06-18 10:10:46 -0500

    primer proyecto python

---
+++ p1.py
@@ -0,0 +1,3 @@
+print("hello world, te digo") # importante print para imprimir en pantalla en este ejemplo un hola mundo #
+print("hello world, te digo 'HOLA'") # aqui comillas dentro de comillas puede ser comillas dobles y dentro comillas sencillas o viceversa #
+print(100+50) # imprime el resultado de una operacion, en este caso no se incorpora dentro de comillas ya que no es un texto #
